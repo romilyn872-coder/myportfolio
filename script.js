1c1c0e55c5e612f7fc521a3720279f6bf970e1fa
romilyn872-coder <romilyn872@gmail.com>
2026-09-22 16:37:25 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+menuBtn.addEventListener("click", function () {
+
+    navLinks.classList.toggle("show");
+
+    const icon = menuBtn.querySelector("i");
+
+    if (navLinks.classList.contains("show")) {
+
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-xmark");
+
+    } else {
+
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars");
+
+    }
+
+});
+
